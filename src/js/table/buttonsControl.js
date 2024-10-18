4c409c973661b3de98c68c1cbd3a9441540d66d0
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-10-18 15:16:25 +0300

    Добавляет анимацию для стола

--- src/js/table/buttonsControl.js
+++ src/js/table/buttonsControl.js
@@ -1,2 +1,32 @@
-const buttonTop = document.querySelector('.ergonomics__table-part--top');
-const buttonBottom = document.querySelector('.ergonomics__table-part--bottom');
+const tablePartTop = document.querySelector('#tablePartTop'); // верхняя часть стола
+const buttonTop = document.querySelector('.ergonomics__button--top'); // кнопка "вверх"
+const buttonBottom = document.querySelector('.ergonomics__button--bottom'); // кнопка "вниз"
+
+let isAnimating = false; // происходит ли сейчас анимация?
+
+// слушаем клик кнопки "вверх"
+buttonTop.addEventListener('click', () => {
+  if (isAnimating) return;
+
+  tablePartTop.classList.remove('translate-bottom');
+  tablePartTop.classList.add('translate-top');
+});
+
+// слушаем клик кнопки "вниз"
+buttonBottom.addEventListener('click', () => {
+  // если происходит анимация или верхняя часть стола не имеет класса "translate-top"
+  if (isAnimating || !tablePartTop.classList.contains('translate-top')) return;
+
+  tablePartTop.classList.remove('translate-top');
+  tablePartTop.classList.add('translate-bottom');
+});
+
+// слушаем начало анимации
+tablePartTop.addEventListener('animationstart', () => {
+  isAnimating = true;
+});
+
+// слушаем конец анимации
+tablePartTop.addEventListener('animationend', () => {
+  isAnimating = false;
+});
